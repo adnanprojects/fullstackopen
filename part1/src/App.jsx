1f18a36faf4948dafb60793aa25676b9f8bd6623
adnanprojects <adnanchaudhary99@gmail.com>
2024-04-01 23:37:31 +0530

    Update App

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const now = new Date();
+  const a = 20;
+  const b = 10;
+  console.log(now, a + b);
   return (
     <>
-      ,<h1>Hello World!</h1>
+      <div>
+        <p>Hello world, it is {now.toDateString()}</p>
+        <p>{a} plus {b} is {a + b}</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
