434b83dda2f0e052784eacee1378c3dc1d9eba76
adnanprojects <adnanchaudhary99@gmail.com>
2024-04-05 22:16:51 +0530

    Update App

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,16 +1,16 @@
 function App() {
 
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
 
   return (
     <>
-      <h1>{course}</h1>
+      {/* <h1>{course}</h1>
       <p>
         {part1} {exercises1}
       </p>
@@ -20,7 +20,10 @@ function App() {
       <p>
         {part3} {exercises3}
       </p>
-      <p>The total exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>The total exercises {exercises1 + exercises2 + exercises3}</p> */}
+      <Header course={course} />
+      <Content />
+      <Total />
     </>
   )
 }
