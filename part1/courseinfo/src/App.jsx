de50e245efd5c65530b848f8fedf8db2acd03405
adnanprojects <adnanchaudhary99@gmail.com>
2024-04-09 11:15:55 +0530

    Update App

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
 
   const Header = (props) => {
-    console.log(props, 'header');
-    return <h1>{props.course}</h1>;
+    console.log(props.course, 'header');
+    return <h1>{props.course.name}</h1>;
   }
 
   const Content = (props) => {
@@ -23,27 +23,29 @@ function App() {
     </>
   }
 
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
