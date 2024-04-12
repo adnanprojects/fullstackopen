deb11e3dfb8af05477f42095fb7e3d3b412a1860
adnanprojects <adnanchaudhary99@gmail.com>
2024-04-12 20:33:40 +0530

    Add Neutral & Bad counter

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,19 +2,33 @@ import { useState } from 'react';
 
 const App = () => {
   const [good, setGood] = useState(0);
+  const [neutal, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGood = () => {
     setGood(prevGood => prevGood + 1);
+  }
+
+  const handleNeutarl = () => {
+    setNeutral(prevNeutal => prevNeutal + 1);
+  }
+
+  const handleBad = () => {
+    setBad(prevBad => prevBad + 1);
   }
 
   return (
     <>
       <h1>give feedback</h1>
 
-      <button onClick={handleGood}>Good</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutarl}>neural</button>
+      <button onClick={handleBad}>bad</button>
 
       <h2>statistics</h2>
-      <h3>good {good}</h3>
+      <p>good {good}</p>
+      <p>neutral {neutal}</p>
+      <p>bad {bad}</p>
     </>
   );
 }
