377a07f85a2ae572af9b5a60f0567660d14248cf
adnanprojects <adnanchaudhary99@gmail.com>
2024-04-12 20:22:53 +0530

    Add good couter

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react';
 
 const App = () => {
   const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(prevGood => prevGood + 1);
+  }
 
   return (
     <>
-      Code here
+      <h1>give feedback</h1>
+
+      <button onClick={handleGood}>Good</button>
+
+      <h2>statistics</h2>
+      <h3>good {good}</h3>
     </>
   );
 }
