4aa6639fa0e627675575a0577a84727a8daa0638
adnanprojects <adnanchaudhary99@gmail.com>
2024-04-18 23:47:28 +0530

    Break components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react';
 
+const Statistics = (props) => {
+  const { good, neutal, bad } = props;
+  return (
+    <>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutal}</p>
+      <p>bad {bad}</p>
+      <p>all {good + bad + neutal}</p>
+      <p>average {(good - bad) / (good + neutal + bad)}</p>
+      <p>positive {good / (good + neutal + bad) * 100}%</p>
+    </>
+  );
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutal, setNeutral] = useState(0);
@@ -17,10 +32,6 @@ const App = () => {
     setBad(prevBad => prevBad + 1);
   }
 
-  const Statistics = (props) => {
-    //  
-  }
-
   return (
     <>
       <h1>give feedback</h1>
@@ -29,13 +40,7 @@ const App = () => {
       <button onClick={handleNeutarl}>neural</button>
       <button onClick={handleBad}>bad</button>
 
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutal}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutal}</p>
-      <p>average {(good - bad) / (good + neutal + bad)}</p>
-      <p>positive {good / (good + neutal + bad) * 100}%</p>
+      <Statistics good={good} neutal={neutal} bad={bad} />
     </>
   );
 }
